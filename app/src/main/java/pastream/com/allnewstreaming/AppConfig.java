976241a428c274c691e89d1976bbce75546d1d44
ZetBrush <zetbrush@gmail.com>
2015-09-04 09:12:45 +0400

    Initial commit with working camerea live stream

---
+++ app/src/main/java/pastream/com/allnewstreaming/AppConfig.java
@@ -0,0 +1,10 @@
+package pastream.com.allnewstreaming;
+
+/**
+ * Created by Arman on 9/3/15.
+ */
+public  class AppConfig {
+	public static final String STREAM_URL = "rtsp://192.168.4.242:1935/live/myStream";
+	public static final String PUBLISHER_USERNAME = "";
+	public static final String PUBLISHER_PASSWORD = "";
+}
